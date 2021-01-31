33b73755941f88db35b448a214c40098eb052bd9
MrGio7 <lashagiorgi1233@gmail.com>
2021-01-31 08:23:01 -0800

    added traps

--- rooms.py
+++ rooms.py
@@ -1,3 +1,5 @@
+import random
+
 class Rooms:
     def __init__(self, doors) -> None:
         self.doors = doors
@@ -20,7 +22,12 @@ class Rooms:
                 if selection >= len(self.doors) + 1:
                     print("You can`t go back, please select the door")
                 elif selection > 0 and selection <= len(self.doors):
-                    return (f"\n You have entered {self.doors[selection - 1]} and you see some other doors, please select next door")
+                    rnd = random.randrange(1, len(self.doors) + 1)
+                    if selection == rnd:
+                        return (f"\n You have entered {self.doors[selection - 1]} and you see some other doors, please select next door")
+                    else:
+                        print(f"\n You have entered TRAP ROOM, RIP, correct path was {str(rnd)} please try again")
+                        exit()
 
             except ValueError:
                 print("Please enter your choice as a number.")
